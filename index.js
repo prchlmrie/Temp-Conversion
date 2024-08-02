e69251111f589e1c8140f8f74b3a9b78ae01a804
Chei <107986749+prchlmrie@users.noreply.github.com>
2024-08-02 15:01:37 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const textNum = document.getElementById(`textNum`);
+const toFahrenheit = document.getElementById(`toFahrenheit`);
+const toCelcius = document.getElementById(`toCelcius`);
+const result = document.getElementById(`result`);
+
+let temp;
+
+function convert(){
+
+    if(toFahrenheit.checked){
+        temp = Number(textNum.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(toCelcius.checked){
+        temp = Number(textNum.value);
+        temp = (temp - 32) *  (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = `Select a unit`;
+    }
+
+}
